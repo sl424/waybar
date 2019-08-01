7efcb8ae071c5756fffa5715986db7d53cba3df4
Hunter Peavey <vrywjvgviuotxeioqipx@disroot.org>
2019-08-01 12:57:02 -0700

    Rely more on constants, remove unnecessary function, reduce decimal display precision for wattage

--- battery/battery.c
+++ battery/battery.c
@@ -7,6 +7,13 @@
 
 // Constants
 #define mW_TO_WATTS_FACTOR 1000000
+#define STR_BUFFER_LEN 15
+
+// Battery paths
+#define PATH_POWER_NOW "/sys/class/power_supply/BAT0/power_now"
+#define PATH_ENERGY_FULL "/sys/class/power_supply/BAT0/energy_full"
+#define PATH_ENERGY_NOW "/sys/class/power_supply/BAT0/energy_now"
+#define PATH_STATE "/sys/class/power_supply/BAT0/status"
 
 // Functions
 void construct_path(char endOfPath[], char finalPath[]);
@@ -16,29 +23,29 @@ void read_string_from_file(char fileToOpen[], char contentOfFile[]);
 int main()
 {	
 	// 1. Get current discharge in milliwatts
-	float currentDischarge = read_float_from_file("/sys/class/power_supply/BAT0/power_now");
+	float currentDischarge = read_float_from_file(PATH_POWER_NOW);
 	
 	// 1a. Convert to watts
 	currentDischarge = currentDischarge / mW_TO_WATTS_FACTOR;
 	
 	// 1b. Make watt display string
-	char wattDisplayString[10];
+	char wattDisplayString[STR_BUFFER_LEN];
 	
-	snprintf(wattDisplayString, sizeof(wattDisplayString), "%0.1fW ", 
+	snprintf(wattDisplayString, sizeof(wattDisplayString), "%0.0fW ", 
 	currentDischarge); 
 	
 	// 2. Get current battery state and percentage
-	char batteryState[25];
+	char batteryState[STR_BUFFER_LEN];
 	
-	read_string_from_file("/sys/class/power_supply/BAT0/status", batteryState);
+	read_string_from_file(PATH_STATE, batteryState);
 	
 	// 2a. Convert batteryState to lowercase as CSS expects a lowercase value
 	for(int i = 0; batteryState[i]; i++){
 		batteryState[i] = tolower(batteryState[i]);
 	}
 	
-	float batteryPercentage = read_float_from_file("/sys/class/power_supply/BAT0/energy_now") 
-	/ read_float_from_file("/sys/class/power_supply/BAT0/energy_full") * 100;
+	float batteryPercentage = read_float_from_file(PATH_ENERGY_NOW) / 
+	read_float_from_file(PATH_ENERGY_FULL) * 100;
 	
 	// 3. Determine battery icon to use based on battery state and percentage
 	char *batteryIcon;
@@ -68,7 +75,7 @@ int main()
 	}
 	
 	// Print final output
-	char finalOutput[15];
+	char finalOutput[STR_BUFFER_LEN];
 	
 	snprintf(finalOutput, sizeof(finalOutput), "%0.0f%% %s%s%s", 
 	batteryPercentage, wattDisplayString, batteryIcon, batteryStateIcon);
@@ -76,11 +83,6 @@ int main()
 	printf("{\"text\": \"%s\", \"class\": \"%s\"}", finalOutput, batteryState);
 	
 	return 0;
-}
-
-void construct_path(char endOfPath[], char finalPath[])
-{
-	
 }
 
 float read_float_from_file(char fileToOpen[])
